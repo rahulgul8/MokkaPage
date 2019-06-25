eed08bf12a700e059cdf4f8a698c80ad6dace3d1
rahul <rahulgul8@gmail.com>
2019-06-26 00:02:42 +0530

    fixing the return statement

--- functions/index.js
+++ functions/index.js
@@ -6,7 +6,7 @@ const db = admin.firestore()
 
 exports.getQuestions = functions.https.onRequest((request, response) => {
     var stuff = [];
-    db.collection("/questions").get().then((querySnapshot) => {
+    return db.collection("/questions").get().then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
             stuff.push(doc.data());
         });
@@ -16,10 +16,10 @@ exports.getQuestions = functions.https.onRequest((request, response) => {
 
 exports.updateUserResponse = functions.https.onRequest((request, response) => {
     if (request.data) {
-        db.collection("/userResponse").add(request.data)
+        return db.collection("/userResponse").add(request.data)
             .then((d) => response.send(d.id)).catch((e) => response.send(e));
     } else {
-        response.send("no response");
+        return response.send("no response");
     }
 });
 
@@ -27,7 +27,7 @@ exports.updatePlayerResponse = functions.https.onRequest((request, response) => 
     if (request.data.id && request.data.name && request.data.score) {
         const respRef = db.collection('/userResponse').doc(request.data.id);
         const usrResponse = { name: request.data.name, score: request.data.score };
-        db.runTransaction(transaction => {
+        return db.runTransaction(transaction => {
             // This code may get re-run multiple times if there are conflicts.
             return transaction.get(respRef).then(doc => {
                 if (!doc.data().responses) {
@@ -43,23 +43,23 @@ exports.updatePlayerResponse = functions.https.onRequest((request, response) => 
             });
         }).then(() => {
             console.log("Transaction successfully committed!");
-            return response.send(1)
+            return response.send(200)
         }).catch((error) => {
             console.log("Transaction failed: ", error);
-            response.send(error)
+            return response.send(error)
         });
     } else {
-        response.send("no response");
+        return response.send("no response");
     }
 });
 
 exports.getPlayerQuestionsAndResponses = functions.https.onRequest((request, response) => {
     if (request.id) {
-        db.collection("/userResponse").doc(request.id).get().then((doc) => {
+        return db.collection("/userResponse").doc(request.id).get().then((doc) => {
             return response.send({ questions: doc.questions, responses: doc.responses });
         }).catch(e => response.send(e));
     } else {
         console.log('received invalid request: ' + request);
-        response.send(0);
+        return response.send(500);
     }
 });
